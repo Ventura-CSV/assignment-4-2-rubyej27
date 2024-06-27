92610e9e49f419525f2d4e8b94bb643573f8dadb
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-06-27 19:38:37 -0400

    4-2 commit

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ def main():
     Code Your Program here
     ########################################
     """
+    result = [a1, a2]
+    for i in range(2, N):
+        a3 = result[-1] + result[-2]
+        result.append(a3)
+    print(result)
+    
+    
 
     ########################################
     # Do not delete the return statement
